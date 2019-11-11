e97de559467ab0066c681c29ebc6986d90c3479d
daniel makarzec <danielmakarzec@gmail.com>
2019-11-11 20:38:57 +0000

    challenge completed

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    abc = ('A'..'Z').to_a
+    9.times { @letters << abc.sample }
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
